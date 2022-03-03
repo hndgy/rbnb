3516a63a4857e9bb66edd45f4b164b2c8898ba45
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-03-03 18:15:25 +0100

    Post, GetById User sans Keycloak

--- utilisateur-service/Models/UserServiceDbContext.cs
+++ utilisateur-service/Models/UserServiceDbContext.cs
@@ -4,9 +4,26 @@ using utilisateur_service.Entities;
 namespace utilisateur_service.Models;
 public class UserServiceDbContext : DbContext
 {
+
+    public UserServiceDbContext()
+    {
+    }
     public UserServiceDbContext(DbContextOptions<UserServiceDbContext> options)
         : base(options)
-    { }
+    {
+    }
 
     public DbSet<Utilisateur> Utilisateurs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=UtilisateurService;User=user;Password=1234";
+            optionsBuilder.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString)
+            );
+        }
+    }
 }
