a88891e60e798fca4caf8bb3b2f103552092fe76
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-03-15 10:58:26 +0100

    Ajout docker compose général

---
+++ dump/onstart-utlisateur-service-mysql.sql
@@ -0,0 +1,15 @@
+GRANT ALL PRIVILEGES ON *.* TO 'user'
+WITH
+GRANT OPTION;
+
+flush privileges;
+
+
+CREATE TABLE UtilisateurService.Utilisateurs
+(
+    Id VARCHAR(100) NOT NULL PRIMARY KEY,
+    Nom VARCHAR(50) NULL,
+    Prenom VARCHAR(50) NULL,
+    Mail VARCHAR(100) NULL,
+    UserName VARCHAR(50) NULL
+);
