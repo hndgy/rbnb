51dfdbed7b8774674c4d9fbecd0229596dd63a28
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-03-16 10:41:44 +0100

    Changement de projet : initialition du projet en monolithique

---
+++ rbnb-monolithique/src/main/java/fr/orleans/univ/miage/m2/rbnbmonolithique/RbnbMonolithiqueApplication.java
@@ -0,0 +1,13 @@
+package fr.orleans.univ.miage.m2.rbnbmonolithique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbnbMonolithiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbnbMonolithiqueApplication.class, args);
+	}
+
+}
---
+++ rbnb-monolithique/src/test/java/fr/orleans/univ/miage/m2/rbnbmonolithique/RbnbMonolithiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orleans.univ.miage.m2.rbnbmonolithique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbnbMonolithiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
