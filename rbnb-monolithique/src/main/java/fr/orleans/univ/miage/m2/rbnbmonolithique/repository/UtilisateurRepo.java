8c0c3f03a0c2c80ff4fbcfe496036ec549cbd6a4
theaupty <theaupaty@icloud.com>
2022-03-16 12:25:58 +0100

    repo0.2

--- rbnb-monolithique/src/main/java/fr/orleans/univ/miage/m2/rbnbmonolithique/repository/UtilisateurRepo.java
+++ rbnb-monolithique/src/main/java/fr/orleans/univ/miage/m2/rbnbmonolithique/repository/UtilisateurRepo.java
@@ -4,6 +4,7 @@ import fr.orleans.univ.miage.m2.rbnbmonolithique.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UtilisateurRepo extends JpaRepository<Utilisateur, Long> {
+
     Utilisateur getUtilisateurById(Long id);
-    Utilisateur getUtilisateurByPrenomAndEmail(String prenom, String email);
+
 }
