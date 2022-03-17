8f360bbef7d19b30520d223afd55e1dcaca6c4eb
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-03-17 16:27:48 +0100

    Fix CommentaireRepo

--- rbnb-monolithique/src/main/java/fr/orleans/univ/miage/m2/rbnbmonolithique/repository/CommentaireRepo.java
+++ rbnb-monolithique/src/main/java/fr/orleans/univ/miage/m2/rbnbmonolithique/repository/CommentaireRepo.java
@@ -2,11 +2,12 @@ package fr.orleans.univ.miage.m2.rbnbmonolithique.repository;
 
 import fr.orleans.univ.miage.m2.rbnbmonolithique.entity.Commentaire;
 import fr.orleans.univ.miage.m2.rbnbmonolithique.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-public interface CommentaireRepo extends MongoRepository<Commentaire, String> {
+public interface CommentaireRepo extends JpaRepository<Commentaire, Long> {
 
-    @Query(value = "{ }")
-    Commentaire getCommentaireByReservation(String idReservation);
+
+    Commentaire getCommentaireByIdReservation(String idReservation);
 }
