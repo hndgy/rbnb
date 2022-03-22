d65bc602e6caca985733953e286ac4d700fb6961
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-03-22 18:55:51 +0100

    Ajout du discovery

--- rbnb-monolithique/src/main/java/fr/orleans/univ/miage/m2/rbnbmonolithique/RbnbMonolithiqueApplication.java
+++ rbnb-monolithique/src/main/java/fr/orleans/univ/miage/m2/rbnbmonolithique/RbnbMonolithiqueApplication.java
@@ -3,9 +3,10 @@ package fr.orleans.univ.miage.m2.rbnbmonolithique;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RbnbMonolithiqueApplication {
 
 	public static void main(String[] args) {
