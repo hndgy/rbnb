263220e2112dbb872931a9327d168f30843a9a9d
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-03-22 17:08:59 +0100

    Configure api-gateway

--- api-gateway/src/main/java/fr/orleans/univ/miage/interop/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/fr/orleans/univ/miage/interop/apigateway/ApiGatewayApplication.java
@@ -1,9 +1,20 @@
 package fr.orleans.univ.miage.interop.apigateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.support.ServiceUnavailableException;
+import org.springframework.context.annotation.Bean;
+
+import java.net.URI;
+import java.util.Optional;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
