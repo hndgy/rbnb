11dda42eeb5cc05bf3dc0b7f0e3a938e37f37acd
Maxime Rousset <newmazien@gmail.com>
2022-03-24 09:50:03 +0100

    Début ReservationService

--- rbnb-reservation-service/src/main/java/fr/orleans/univ/miage/m2/rbnbreservationservice/service/ReservationService.java
+++ rbnb-reservation-service/src/main/java/fr/orleans/univ/miage/m2/rbnbreservationservice/service/ReservationService.java
@@ -3,6 +3,7 @@ package fr.orleans.univ.miage.m2.rbnbreservationservice.service;
 import fr.orleans.univ.miage.m2.rbnbreservationservice.entity.Reservation;
 import fr.orleans.univ.miage.m2.rbnbreservationservice.service.exceptions.LogementsIndisponibleException;
 import fr.orleans.univ.miage.m2.rbnbreservationservice.service.exceptions.NbVoyagageurIncorrecteException;
+import fr.orleans.univ.miage.m2.rbnbreservationservice.service.exceptions.ReservationIntrouvableException;
 
 import java.util.Collection;
 import java.util.Date;
@@ -12,7 +13,7 @@ public interface ReservationService {
 
     Collection<Reservation> getReservationsByVoyageur(Long idVoyageur);
     Reservation createReservation(Reservation reservation);
-    void updateNbVoyageursReservation(String idRerservation, int nbVoyageurs) throws NbVoyagageurIncorrecteException;
+    void updateNbVoyageursReservation(String idRerservation, int nbVoyageurs) throws NbVoyagageurIncorrecteException, ReservationIntrouvableException;
     void updateDateReservation(String idReservation, Date dateDebut, Date dateFin) throws LogementsIndisponibleException;
-    void annulerReservation(String idReservation);
+    void annulerReservation(String idReservation) throws ReservationIntrouvableException;
 }
--- rbnb-reservation-service/src/main/java/fr/orleans/univ/miage/m2/rbnbreservationservice/service/ReservationServiceImpl.java
+++ rbnb-reservation-service/src/main/java/fr/orleans/univ/miage/m2/rbnbreservationservice/service/ReservationServiceImpl.java
@@ -1,13 +1,26 @@
 package fr.orleans.univ.miage.m2.rbnbreservationservice.service;
 
 import fr.orleans.univ.miage.m2.rbnbreservationservice.entity.Reservation;
+import fr.orleans.univ.miage.m2.rbnbreservationservice.repository.DisponibiliteRepo;
+import fr.orleans.univ.miage.m2.rbnbreservationservice.repository.ReservationRepo;
 import fr.orleans.univ.miage.m2.rbnbreservationservice.service.exceptions.LogementsIndisponibleException;
 import fr.orleans.univ.miage.m2.rbnbreservationservice.service.exceptions.NbVoyagageurIncorrecteException;
+import fr.orleans.univ.miage.m2.rbnbreservationservice.service.exceptions.ReservationIntrouvableException;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.Optional;
 
 public class ReservationServiceImpl implements ReservationService {
+
+    private final ReservationRepo reservationRepo;
+    private final DisponibiliteRepo disponibiliteRepo;
+
+    public ReservationServiceImpl(ReservationRepo reservationRepo, DisponibiliteRepo disponibiliteRepo) {
+        this.reservationRepo = reservationRepo;
+        this.disponibiliteRepo = disponibiliteRepo;
+    }
+
     @Override
     public Collection<Reservation> getReservationsByHote(Long idHote) {
         return null;
@@ -15,8 +28,9 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public Collection<Reservation> getReservationsByVoyageur(Long idVoyageur) {
-        return null;
+        return reservationRepo.findAllReservationsByIdClient(idVoyageur);
     }
+
 
     @Override
     public Reservation createReservation(Reservation reservation) {
@@ -24,8 +38,14 @@ public class ReservationServiceImpl implements ReservationService {
     }
 
     @Override
-    public void updateNbVoyageursReservation(String idRerservation, int nbVoyageurs) throws NbVoyagageurIncorrecteException {
-
+    public void updateNbVoyageursReservation(String idReservation, int nbVoyageurs) throws NbVoyagageurIncorrecteException, ReservationIntrouvableException {
+        Optional<Reservation> reservation;
+        reservation = reservationRepo.findById(idReservation);
+        if (reservation.isPresent()) {
+            reservation.get().setNbVoyageurs(nbVoyageurs);
+            reservationRepo.save(reservation.get());
+        }
+        else throw new ReservationIntrouvableException();
     }
 
     @Override
@@ -34,7 +54,12 @@ public class ReservationServiceImpl implements ReservationService {
     }
 
     @Override
-    public void annulerReservation(String idReservation) {
-
+    public void annulerReservation(String idReservation) throws ReservationIntrouvableException {
+        Optional<Reservation> reservation;
+        reservation = reservationRepo.findById(idReservation);
+        if (reservation.isPresent()) {
+            reservationRepo.deleteById(idReservation);
+        }
+        else throw new ReservationIntrouvableException();
     }
 }
