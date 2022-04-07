00e49c42b20e108b83d2415b044cbbc07b816a0f
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-04-07 13:47:32 +0200

    mailing service

--- rbnb-mailing-service/src/main/java/fr/orleans/univ/miage/m2/rbnbmailingservice/services/MailingService.java
+++ rbnb-mailing-service/src/main/java/fr/orleans/univ/miage/m2/rbnbmailingservice/services/MailingService.java
@@ -1,5 +1,28 @@
 package fr.orleans.univ.miage.m2.rbnbmailingservice.services;
 
-public class MailingService {
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+public class MailingService {
+    public static final String FROM_EMAIL = "contact@rbnb.com";
+    private final JavaMailSender mailSender;
+
+    public MailingService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(FROM_EMAIL);
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("mail sent : " + message.toString());
+
+    }
 }
