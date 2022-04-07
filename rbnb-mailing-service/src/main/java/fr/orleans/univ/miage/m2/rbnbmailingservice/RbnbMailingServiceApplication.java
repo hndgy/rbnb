559f1adac70e104350edce89e6ae6488793e7392
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-04-07 13:24:17 +0200

    Init Mailing service

---
+++ rbnb-mailing-service/src/main/java/fr/orleans/univ/miage/m2/rbnbmailingservice/RbnbMailingServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.orleans.univ.miage.m2.rbnbmailingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbnbMailingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbnbMailingServiceApplication.class, args);
+	}
+
+}
---
+++ rbnb-mailing-service/src/test/java/fr/orleans/univ/miage/m2/rbnbmailingservice/RbnbMailingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orleans.univ.miage.m2.rbnbmailingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbnbMailingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
