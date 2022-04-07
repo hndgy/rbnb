7760b2b8bc6e40487b9595ab74a777a3914d745e
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-04-07 14:34:14 +0200

    Missing file last commit

---
+++ Rbnb.UtilisateurService/Dtos/MailMessage.cs
@@ -0,0 +1,9 @@
+namespace Rbnb.UtilisateuService.Dtos
+{
+    public class MailMessage
+    {
+        public string toEmail{get; set;}
+        public string subject{get; set;}
+        public string content{get; set;}
+    }
+}
