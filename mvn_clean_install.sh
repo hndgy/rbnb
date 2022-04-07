704fdbc5dcd408933c926fd3c4680047af899a59
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-04-07 15:12:32 +0200

    Automatisation des mvn clean install

---
+++ mvn_clean_install.sh
@@ -0,0 +1,15 @@
+cd ./rbnb-logement-service/;
+mvn clean install;
+cd ..;
+
+cd ./rbnb-review-service/;
+mvn clean install;
+cd ..;
+
+cd ./rbnb-mailing-service/;
+mvn clean install;
+cd ..;
+
+cd ./rbnb-reservation-service/;
+mvn clean install;
+cd ..;
