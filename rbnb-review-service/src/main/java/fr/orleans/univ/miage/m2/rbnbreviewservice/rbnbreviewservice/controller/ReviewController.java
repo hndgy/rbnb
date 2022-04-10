39c831ca95f4aa41230287595632a1c890df39ef
theaupty <theaupaty@icloud.com>
2022-04-10 15:56:44 +0200

    roles

--- rbnb-review-service/src/main/java/fr/orleans/univ/miage/m2/rbnbreviewservice/rbnbreviewservice/controller/ReviewController.java
+++ rbnb-review-service/src/main/java/fr/orleans/univ/miage/m2/rbnbreviewservice/rbnbreviewservice/controller/ReviewController.java
@@ -7,7 +7,6 @@ import fr.orleans.univ.miage.m2.rbnbreviewservice.rbnbreviewservice.entity.Prest
 import fr.orleans.univ.miage.m2.rbnbreviewservice.rbnbreviewservice.exception.PrestationNotFoundException;
 import fr.orleans.univ.miage.m2.rbnbreviewservice.rbnbreviewservice.exception.ReviewNotFoundException;
 import fr.orleans.univ.miage.m2.rbnbreviewservice.rbnbreviewservice.service.ReviewService;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,14 +27,14 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @RolesAllowed("ADMIN")
+    @RolesAllowed("USER")
     @GetMapping("/{id}")
     public ResponseEntity<ReviewDto> getReviewById(@PathVariable(name = "id")Long id, @RequestHeader(name = "Authorization")String token) throws ReviewNotFoundException {
         ReviewDto reviewDto = reviewService.getReviewById(id, token);
         return ResponseEntity.ok().body(reviewDto);
     }
 
-    @RolesAllowed({"USER", "ADMIN"})
+    @RolesAllowed({"USER"})
     @PostMapping
     public ResponseEntity<ReviewDto> createReview(@RequestBody ReviewCreationDto reviewCreationDto){
         ReviewDto reviewDto = reviewService.createReview(reviewCreationDto);
@@ -55,25 +54,26 @@ public class ReviewController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @RolesAllowed({"USER", "ADMIN"})
+    @RolesAllowed({"USER"})
     @GetMapping("/utilisateur")
     public ResponseEntity<List<ReviewDto>> getAllReviewByUtilisateur(Principal principal){
         return ResponseEntity.ok().body(reviewService.getAllByUtilisateur(principal.getName()));
     }
 
-    @RolesAllowed({"HOTE", "ADMIN"})
+    @RolesAllowed({"HOTE"})
     @GetMapping("/logement/{id}")
     public ResponseEntity<List<ReviewDto>> getAllReviewByLogement(@PathVariable(name = "id") Long id){
         return ResponseEntity.ok().body(reviewService.getAllByLogement(id));
     }
 
-    @RolesAllowed({"USER", "ADMIN"})
+    @RolesAllowed({"USER"})
     @PostMapping("/notation")
     public ResponseEntity<NotationDto> createNotation(@RequestBody NotationDto notationDto){
         NotationDto notation = reviewService.createNotation(notationDto);
         return new ResponseEntity<>(notation,HttpStatus.CREATED);
     }
 
+    @RolesAllowed({"USER"})
     @GetMapping("/prestations")
     public ResponseEntity<List<Prestation>> getAllPrestations(){
         List<Prestation> prestations = reviewService.getAllPrestation();
